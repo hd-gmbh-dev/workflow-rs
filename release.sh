75be2ea28f283f14099c3ec0e080060d90768533
Jürgen Seitz <juergen.seitz@h-d-gmbh.de>
2023-10-27 03:57:19 +0200

    build: prepare release 0.0.9

--- release.sh
+++ release.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cargo set-version --workspace $1
+pnpm --filter ./wasm/parser/helper build
 wasm-pack build wasm/parser --scope wfrs --target nodejs --release
 wasm-pack build wasm/runtime --scope wfrs --target web --release
 npm version $1 --include-workspace-root -ws --no-git-tag-version --allow-same-version --no-workspaces-update
+
 cp wasm/parser/package_json/package.json wasm/parser/pkg
 git add .
 git commit -m "build: prepare release $1"
