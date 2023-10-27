a716257c1f18ca39b5a65b45a1a66014c98f5214
Jürgen Seitz <juergen.seitz@h-d-gmbh.de>
2023-10-27 03:20:35 +0200

    build: prepare release 0.0.2

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cargo set-version $1
+npm version $1 --include-workspace-root -ws --no-git-tag-version --allow-same-version --no-workspaces-update
+git add .
+git commit -m "build: prepare release $1"
+git push
+git tag $1
+git push -u origin $1
+
+cargo publish -p wfrs-model
+cargo publish -p wfrs-validator
+cargo publish -p wfrs-engine
+pnpm publish --recursive
