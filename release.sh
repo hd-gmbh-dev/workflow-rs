c103ef1895b109de7d5b4fc966bd906c1b100d97
Jürgen Seitz <juergen.seitz@h-d-gmbh.de>
2023-10-30 10:18:57 +0100

    build: prepare release v0.0.10

--- release.sh
+++ release.sh
@@ -5,15 +5,13 @@ pnpm --filter ./wasm/parser/helper build
 wasm-pack build wasm/parser --scope wfrs --target nodejs --release
 wasm-pack build wasm/runtime --scope wfrs --target web --release
 npm version $1 --include-workspace-root -ws --no-git-tag-version --allow-same-version --no-workspaces-update
-
 cp wasm/parser/package_json/package.json wasm/parser/pkg
 git add .
-git commit -m "build: prepare release $1"
+git commit -m "build: prepare release v$1"
 git push
-git tag $1
-git push -u origin $1
-
-cargo publish -p wfrs-model
-cargo publish -p wfrs-validator
-cargo publish -p wfrs-engine
-pnpm publish --recursive --access public
+git tag v$1
+git push -u origin v$1
+# cargo publish -p wfrs-model
+# cargo publish -p wfrs-validator
+# cargo publish -p wfrs-engine
+# pnpm publish --recursive --access public
