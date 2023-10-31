140d587c403e993796aead8ef9ae2c8fa08beabb
Jürgen Seitz <juergen.seitz@h-d-gmbh.de>
2023-10-31 21:42:36 +0100

    build: prepare release v0.0.12

--- release.sh
+++ release.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# cargo set-version --workspace $1
-# npm version $1 --include-workspace-root -ws --no-git-tag-version --allow-same-version --no-workspaces-update
-# pnpm i
-# pnpm build
+cargo set-version --workspace $1
+npm version $1 --include-workspace-root -ws --no-git-tag-version --allow-same-version --no-workspaces-update
+pnpm i
+pnpm build
 
 git add .
 git commit -m "build: prepare release v$1"
 git push
-git tag v$1
-git push -u origin v$1
+# git tag v$1
+# git push -u origin v$1
 
-# pnpm publish --recursive --access public
-# cargo publish -p wfrs-model
-# cargo publish -p wfrs-validator
-# cargo publish -p wfrs-engine
+pnpm publish --recursive --access public --no-git-checks
+cargo publish -p wfrs-model
+cargo publish -p wfrs-validator
+cargo publish -p wfrs-engine
